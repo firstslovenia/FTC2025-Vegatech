c001685cdbe6c485ddc1a828020d3496488305d5
Domen <domen.podgorsek@students.vegova.si>
2025-02-14 18:32:33 +0100

    Spremenili imena smremenljivk in zaceli s pisanjem kode za lifter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lifter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lifter.java
@@ -2,37 +2,38 @@ package org.firstinspires.ftc.teamcode;
 
 public class Lifter {
 
-    private static double wantedLocation;
-    private static final int maxPosition = 250;
-    private static double zero = 0;
-    private static boolean pressed = false;
+    private static double rokaWantedLocation;
+    private static final int rokaMaxPosition = 250;
+    private static double rokaZero = 0;
+    private static boolean rokaPressed = false;
+
 
     public static void rokaTilt() {
 //        float powerToSet = InputMapper.leftTrigger - InputMapper.rightTrigger;
-        double currentLocation = -((-Hardware.rokaLeft.getCurrentPosition() + Hardware.rokaRight.getCurrentPosition()) * 0.625) - zero;
+        double currentLocation = -((-Hardware.rokaLeft.getCurrentPosition() + Hardware.rokaRight.getCurrentPosition()) * 0.625) - rokaZero;
         BetterTelemetry.print("rokaLeft", Hardware.rokaLeft.getCurrentPosition());
         BetterTelemetry.print("rokaRight", Hardware.rokaRight.getCurrentPosition());
         BetterTelemetry.print("currentLocation", currentLocation);
         double powerToSet = 0;
 
         if (InputMapper.leftBumperPressed) {
-            zero += currentLocation;
-            wantedLocation = 0;
-        } else if (InputMapper.rightBumperPressed && !pressed) {
-            zero -= 10;
-            wantedLocation += 10;
-            pressed = true;
+            rokaZero += currentLocation;
+            rokaWantedLocation = 0;
+        } else if (InputMapper.rightBumperPressed && !rokaPressed) {
+            rokaZero -= 10;
+            rokaWantedLocation += 10;
+            rokaPressed = true;
         } else {
-            wantedLocation += (InputMapper.leftTrigger - InputMapper.rightTrigger);
-            wantedLocation = Math.max(0, Math.min(maxPosition, wantedLocation));
-            powerToSet = Math.pow((wantedLocation - currentLocation) / 10, 3);
+            rokaWantedLocation += (InputMapper.leftTrigger - InputMapper.rightTrigger);
+            rokaWantedLocation = Math.max(0, Math.min(rokaMaxPosition, rokaWantedLocation));
+            powerToSet = Math.pow((rokaWantedLocation - currentLocation) / 10, 3);
         }
 
         if (!InputMapper.rightBumperPressed) {
-            pressed = false;
+            rokaPressed = false;
         }
         //        double powerToSet = ;
-        BetterTelemetry.print("wantedLocation", wantedLocation);
+        BetterTelemetry.print("rokaWantedLocation", rokaWantedLocation);
         BetterTelemetry.print("powertoset", powerToSet);
 //        float powerToSet =
         Hardware.rokaLeft.setPower(powerToSet);
@@ -44,7 +45,16 @@ public class Lifter {
     }
 
     public static void lift() {
-        float powerToSet = boolToInt(InputMapper.crossPressed) - boolToInt(InputMapper.trianglePressed);
+        float powerToSet = -boolToInt(InputMapper.crossPressed) + boolToInt(InputMapper.trianglePressed);
+        double currentLocation = -((Hardware.lifterLeft.getCurrentPosition() + Hardware.lifterRight.getCurrentPosition()) * 0.200892857);
+
+        //lifterWantedLocation += -boolToInt(InputMapper.crossPressed) + boolToInt(InputMapper.trianglePressed);
+        //lifterWantedLocation = Math.max(0, Math.min(lifterMaxPosition, lifterWantedLocation));
+
+        //double powerToSet = Math.pow((rokaWantedLocation - currentLocation) / 10, 3);
+        BetterTelemetry.print("lifterLeft",Hardware.lifterLeft.getCurrentPosition());
+        BetterTelemetry.print("lifterRight",Hardware.lifterRight.getCurrentPosition());
+        BetterTelemetry.print("currentLocation", currentLocation);
         Hardware.lifterLeft.setPower(powerToSet);
         Hardware.lifterRight.setPower(powerToSet);
     }
